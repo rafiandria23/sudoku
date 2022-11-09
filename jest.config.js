63143d622b42550dc823a5087dce7af8ca9bcd8c
Adam Rafiandri <rafiandria23@gmail.com>
2022-11-09 13:16:42 +0700

    test(jest): create Jest config file

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('jest').Config} */
+const config = {
+  verbose: true,
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
+
+module.exports = config;
